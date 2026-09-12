f882a7f9d71a8dccb1c76579baebf53331b5175e
Isaacava <149443075+Isaacava@users.noreply.github.com>
2026-09-12 01:30:50 +0100

    Add Vite environment type declarations

---
+++ src/vite-env.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SOLANA_RPC_URL?: string;
+  readonly VITE_NETWORK?: string;
+  readonly VITE_DEVNET_CASH_MINT?: string;
+  readonly VITE_DEVNET_NVDA_MINT?: string;
+  readonly VITE_DEVNET_AAPL_MINT?: string;
+  readonly VITE_DEVNET_MSFT_MINT?: string;
+  readonly VITE_DEVNET_GOOG_MINT?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
