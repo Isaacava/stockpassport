794ccfb500d2848a45a9d0487a7a07a4949630bc
Isaacava <149443075+Isaacava@users.noreply.github.com>
2026-09-12 16:07:23 +0100

    Initialize Reown AppKit before StockPassport UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import './lib/appkit';
 import './styles.css';
 import './portfolio.css';
 import PortfolioApp from './PortfolioApp';
