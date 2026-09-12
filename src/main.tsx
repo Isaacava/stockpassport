683ce7b0eceb64f50759a401f12a1838bc94e68f
Isaacava <149443075+Isaacava@users.noreply.github.com>
2026-09-12 15:44:45 +0100

    Add StockPassport landing entry screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles.css';
 import App from './AppV2';
+import Landing from './Landing';
+
+function Root() {
+  const [entered, setEntered] = useState(false);
+  return entered ? <App /> : <Landing onEnter={() => setEntered(true)} />;
+}
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 );
